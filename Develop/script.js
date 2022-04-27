d9d457e6428bc98690608e75efa662bd8d7e4c9c
Maia Davis <maia@Maias-MacBook-Air.local>
2022-04-26 22:56:05 -0400

    made more progress with functions and getting text to actually appear in the text box, but still not getting a random string. progress is progress?

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var randomPassword = document.querySelector("aria-label");
+//var randomPassword = document.querySelector("password");
 
 
 
@@ -16,25 +16,21 @@ generateBtn.addEventListener("click", writePassword);
 
 
 //MY CODE
-
-//Password Criteria Variables
-// lowercase letters
-var characters = {
-lowerLetters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'K', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-//uppercase letters
-upperLetters: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-//numbers
-numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-//special characters
-specialChars: ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '<', '>', '?', '/'],
-}
+  //characters for password
+  const alphaLower = "abcdefghijklmnopqrstuvwxyz"
+  const alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numbers = "0123456789";
+  const symbols ="!@#$%^&*_-+=";
 
 
 //generate function upon clicking button
-generatePassword = function() {
+generatePassword = (length, characters) => {
   //length prompt
   var lengthChoice = window.prompt("Please enter a password length between 8 - 128 characters.");
   console.log(lengthChoice);
+  //turn lengthChoice into integer
+  var passLength = parseInt(lengthChoice);
+  console.log(passLength);
   //criteria prompts
   var ulChoice = window.prompt("Would you like to include capital letters?");
   console.log(ulChoice);
@@ -48,20 +44,19 @@ generatePassword = function() {
   var charChoice = window.prompt("Would you like to include special characters?");
   console.log(charChoice);
 
-  //in event of all nos
 
-//if (ulChoice && llChoice && numChoice && charChoice === 'no') {
-  //window.alert("Please try again. Make sure to choose at least one category and enter a numerical length that is between 8 - 128.");
-//}
+  //if all yes
+if (ulChoice && llChoice && numChoice && charChoice == 'yes') {
+    let password = "";
+    let characters = alphaLower += alphaUpper += numbers += symbols;
+    for (let i = 0; i < passLength; i++); {
+      password += characters.substring(
+        Math.floor(Math.random() * characters.length)
+      );
+      console.log(password);
+    };
 
-  //if all uppercase
-if (ulChoice && llChoice && numChoice && charChoice === 'yes') {
-    for (var i = 0; i <=lengthChoice; i++) {
-      var string = characters.JSON.stringify;
-      console.log(characters)
-      randomCharacter = Math.floor(Math.random() * characters.length);
-      randomPassword += characters.substring(randomCharacter, randomCharacter +1);
-      randomPassword.textContent = randomPassowrd
-    }
+      return password;
+      
   }
 }
